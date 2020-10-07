e20cdd16f9c7656166347d519a8019d17117d273
I057386 <I057386@AGRN56583514A.apj.global.corp.sap>
2020-10-07 12:56:19 +0530

    Initial Commit

---
+++ src/main/java/sb/springboot/MyRecipeAppApplication.java
@@ -0,0 +1,13 @@
+package sb.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRecipeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRecipeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sb/sbboot/MyRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sb.sbboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRecipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
