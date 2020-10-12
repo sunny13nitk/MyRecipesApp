99b32b0a2a287cf081062f19d4ac4238d950016d
I057386 <I057386@AGRN56583514A.apj.global.corp.sap>
2020-10-12 16:19:17 +0530

    closes #5.

--- src/main/java/sb/springboot/controllers/IndexController.java
+++ src/main/java/sb/springboot/controllers/IndexController.java
@@ -1,20 +1,36 @@
 package sb.springboot.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import sb.springboot.repository.RecipeRepository;
 
 @Controller
 public class IndexController
 {
 	private final String indexView = "index";
 	
+	private final RecipeRepository recipeRepo;
+	
+	@Autowired
+	public IndexController(
+	        RecipeRepository recipeRepo
+	)
+	{
+		super();
+		this.recipeRepo = recipeRepo;
+	}
+	
 	@GetMapping(
 	    { "/", "" }
 	)
 	public String showIndexPage(
+	        Model model
 	)
 	{
-		
+		model.addAttribute("recipes", recipeRepo.findAll());
 		return indexView;
 	}
 	
